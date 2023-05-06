9e546de5c2e88bda16c64b13221e474bad06e392
istianmws <istianmuhammad@gmail.com>
2023-05-06 16:22:55 +0700

    Fix some variable

--- Assets/duck.cs
+++ Assets/duck.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
-public class duck : MonoBehaviour
+using UnityEngine.Events;
+
+public class Duck : MonoBehaviour
 {
     [SerializeField] bool isMoving;
     [SerializeField, Range(0,1)]  float durasi;
     [SerializeField, Range(0,2)]  float jumpHeight;
     private Vector2 touchStartPosition;
+    public UnityEvent<Vector3> OnJumpEnd;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -99,7 +103,19 @@ public class duck : MonoBehaviour
         // var seq = DOTween.Sequence();
         // seq.Append(transform.DOMoveY(jumpHeight, durasi));
         // seq.Append(transform.DOMoveY(0, durasi));
-        transform.DOJump(transform.position + direction*6f, jumpHeight,1,durasi);
+        transform
+            .DOJump
+            (
+                transform.position + direction*6f, 
+                jumpHeight,
+                1,
+                durasi
+            )
+            .onComplete = BroadCastPositionOnJumpEnd;
         transform.forward = -direction;
+    }
+    private void BroadCastPositionOnJumpEnd()
+    {
+        OnJumpEnd.Invoke(transform.position);
     }
 }
