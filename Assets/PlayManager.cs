f365e24cfe79904f25ef581cb68ecfbe37b72ce1
istianmws <istianmuhammad@gmail.com>
2023-05-06 16:22:22 +0700

    add spawner terrain

--- Assets/PlayManager.cs
+++ Assets/PlayManager.cs
@@ -1,40 +1,113 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayManager : MonoBehaviour
 {
     // [SerializeField] List<Terrain> terrainPrefabList;
-    [SerializeField] Grass grassPreFab;
-    [SerializeField] Road roadPreFab;
-
+    [SerializeField] Duck duck;
+    [SerializeField] List<Terrain> terrainList;
     [SerializeField] int initialGrassCount =  5;
     [SerializeField] int horizontalSize;
-    [SerializeField] int backViewDistance = -5;
+    [SerializeField] int backViewDistance = -7;
     [SerializeField] int forwardViewDistance = 15;
     [SerializeField, Range(0,1)] float treeProbability;
-    private void Start() {
+    Dictionary<int,Terrain> activeTerrainDict = new Dictionary<int, Terrain>(20);
+    [SerializeField] private int travelDistance;
+    public UnityEvent<int,int> OnUpdateTerrainLimit;
+    private void Start()
+    {
         
+
+        //terrain backward
         for (int zPos=backViewDistance; zPos<initialGrassCount;zPos++)
         {
 
-            var grass =Instantiate(grassPreFab);
-            grass.transform.localPosition = new Vector3(0,0,-zPos*6);
-            
-            grass.SetTreePercentage(zPos < -2 ? 1 : 0);
-
-            grass.Generate(horizontalSize);
-        }
-
-        for (int zPos=initialGrassCount; zPos<forwardViewDistance;zPos++)
-        {
-
-            var terrain =Instantiate(roadPreFab);
+            var terrain =Instantiate(terrainList[0]);
             terrain.transform.localPosition = new Vector3(0,0,-zPos*6);
-            
-            // grass.SetTreePercentage(zPos < -2 ? 1 : 0);
+            if (terrain is Grass grass)
+            {
+                grass.SetTreePercentage(zPos < -2 ? 1 : 0);
+                
+            }
 
             terrain.Generate(horizontalSize);
+
+            activeTerrainDict[zPos]=terrain;
         }
+
+        //terrain forward
+        for (int zPos=initialGrassCount; zPos<forwardViewDistance;zPos++)
+        {
+            var terrain = SpawnRandomTerrain(zPos);  
+
+        }
+        // SpawnRandomTerrain(0);
+    }
+
+    private Terrain SpawnRandomTerrain(int zPos)
+    {
+        Terrain terrainCheck = null;
+        int randomIndex;
+        // Terrain terrain = null;
+        for (int z = -1; z >= -3; z--)
+        {
+           var checkPos = (zPos+z);
+           if (terrainCheck == null)
+           {
+                terrainCheck = activeTerrainDict[checkPos];
+                continue;         
+           }
+           else if (terrainCheck.GetType() != activeTerrainDict[checkPos].GetType())
+           {
+                randomIndex = Random.Range(0,terrainList.Count);
+                return SpawnTerrain(terrainList[randomIndex], zPos); 
+           }
+           else
+           {
+                continue;
+           }
+        }
+
+        var candidateTerrain = new List<Terrain>(terrainList);
+        for (int i = 0; i < candidateTerrain.Count; i++)
+        {
+            if (terrainCheck.GetType() == candidateTerrain[i].GetType())
+            {
+                candidateTerrain.Remove(candidateTerrain[i]);
+                break;
+            }
+        }
+
+        randomIndex = Random.Range(0, candidateTerrain.Count);
+        return SpawnTerrain(candidateTerrain[randomIndex], zPos);
+    }
+    public Terrain SpawnTerrain(Terrain terrain, int zPos)
+    {
+        terrain = Instantiate(terrain);
+        terrain.transform.position = new Vector3(0,0,zPos*-6);
+        terrain.Generate(horizontalSize);
+        activeTerrainDict[zPos] = terrain;
+        return terrain;
+    }
+    public void UpdateTravelDistance(Vector3 targetPosition)
+    {
+        if(targetPosition.z < -travelDistance)
+        {
+            travelDistance = -Mathf.CeilToInt(targetPosition.z); 
+            UpdateTerrain();
+        }
+    }
+    public void UpdateTerrain()
+    {
+        var destroyPos = travelDistance/6 - 1 + backViewDistance;
+        Destroy(activeTerrainDict[destroyPos].gameObject);
+        activeTerrainDict.Remove(destroyPos);
+
+        var spawnPos = travelDistance/6 -1 + forwardViewDistance;
+        SpawnRandomTerrain(spawnPos);
+
+        OnUpdateTerrainLimit.Invoke(horizontalSize, travelDistance+backViewDistance);
     }
 }
