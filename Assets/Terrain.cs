024978f30eaa1ebfa35a29b7f6d0995d1df54ebc
istianmws <istianmuhammad@gmail.com>
2023-05-08 15:47:32 +0700

    Add some boundary left&right

--- Assets/Terrain.cs
+++ Assets/Terrain.cs
@@ -21,12 +21,42 @@ protected int horizontalSize;
         {
             SpawnTile(i);
         }
+        // create empty array of gameobjects with 6 elements
+        GameObject[] boundaryTiles = new GameObject[8];
 
-        var leftBoundaryTile = SpawnTile(-limit-1);
-        var rightBoundaryTile = SpawnTile(limit+1);
+        // spawn left boundary tiles
+        for (int i = 1; i <= 4; i++)
+        {
+            boundaryTiles[i] = SpawnTile(-limit - i);
+            DarkenObject(boundaryTiles[i]);
+            boundaryTiles[i+3] = SpawnTile(limit + i);
+            DarkenObject(boundaryTiles[i+3]);
+        }
 
-        DarkenObject(leftBoundaryTile);
-        DarkenObject(rightBoundaryTile);           
+        // // spawn right boundary tiles
+        // for (int i = 1; i <= 3; i++)
+        // {
+        // }
+
+        // darken the left and right boundary tiles
+        // foreach (GameObject tile in boundaryTiles)
+        // {
+        //     DarkenObject(tile);
+        // }
+
+        // var leftBoundaryTile = SpawnTile(-limit-1);
+        // var leftBoundaryTile2 = SpawnTile(-limit-2);
+        // var leftBoundaryTile3 = SpawnTile(-limit-3);
+        // var rightBoundaryTile = SpawnTile(limit+1);
+        // var rightBoundaryTile2 = SpawnTile(limit+2);
+        // var rightBoundaryTile3 = SpawnTile(limit+3);
+        
+        // DarkenObject(leftBoundaryTile);
+        // DarkenObject(leftBoundaryTile2);
+        // DarkenObject(leftBoundaryTile3);
+        // DarkenObject(rightBoundaryTile); 
+        // DarkenObject(rightBoundaryTile2); 
+        // DarkenObject(rightBoundaryTile3);           
     }
 
     private GameObject SpawnTile(int xPos)
